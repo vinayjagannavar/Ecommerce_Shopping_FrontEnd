7b41d279dba6123aec16c778b493f28737d0373e
vinayjagannavar <vjagannavar@gmail.com>
2024-04-06 20:38:47 -0400

    CORS

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-const DEPLOYED='ecommerce-shopping-backend-production.up.railway.app'
+const DEPLOYED='https://ecommerce-shopping-backend-production.up.railway.app'
 const LOCALHOST='http://localhost:5454'
 
 export const API_BASE_URL = DEPLOYED
